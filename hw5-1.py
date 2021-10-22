823d13f3ab13c7b0eb824dbe7eb54fa8f6ba813c
p22crsullivan <p22crsullivan@fairfieldprep.org>
2021-10-22 09:58:38 -0400

    Work on HW 5-1

---
+++ hw5-1.py
@@ -0,0 +1,23 @@
+# Author: CRS 10/22/21
+import random
+random.seed(16)
+# Question 1
+print(random.randint(31, 50))
+
+# Question 2
+print(random.randrange(3, 75, 3))
+
+# Question 3
+print(random.randrange(20, 31, 3))
+
+# Question 4
+print(random.randint(1, 9))
+
+# Question 5
+print(random.randint(1, 21))
+
+# Question 6
+print(random.choice(['cat', 'dog', 'sheep', 'cow', 'chicken', 'pig']))
+
+# Question 7
+print(random.choices([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k=4))
